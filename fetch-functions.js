f663d43fe7234b2d7e32a3b678716450252f7426
Nando <kommandernando@outlook.com>
2025-01-16 16:37:19 -0500

    Added getRandomComic async function

---
+++ fetch-functions.js
@@ -0,0 +1,21 @@
+export const getRandomComic = async () => {
+  // get random number from 1 - 3038
+  const randomComicNumber = Math.floor(Math.random() * 3038) + 1;
+  const url = 'https://xkcd.vercel.app/?comic=${randomComicNumber}';
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Failed to get comic book');
+    }
+    const data = await response.json();
+    const comic = {
+      title: data.title,
+      img: data.img,
+      alt: data.alt,
+    };
+    return comic;
+  } catch (error) {
+    console.warn(error.message);
+    return null;
+  }
+};
