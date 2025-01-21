660e8c681048f1cf1ef51e20b1265a41eb2a0cdf
Nando <kommandernando@outlook.com>
2025-01-21 12:36:01 -0500

    Added generate random comic functionality

--- app/render-functions.js
+++ app/render-functions.js
@@ -1,35 +1,43 @@
-//RENDER COMIC
-export const renderComicInfo = (comicAreaDiv, responseDataObj) => {
-  comicAreaDiv.innerHTML = " ";
-  const { day, img, num, title, transcript, year } = responseDataObj;
+// //RENDER COMIC
+// export const renderComicInfo = (comicAreaDiv, responseDataObj) => {
+//   comicAreaDiv.innerHTML = " ";
+//   const { day, img, num, title, transcript, year } = responseDataObj;
 
-  //RiSet Comics H1
-  const h1 = document.createElement("h1");
-  h1.textContent = "Riset Comics";
+//   //RiSet Comics H1
+//   const h1 = document.createElement("h1");
+//   h1.textContent = "Riset Comics";
 
-  //Comic Title H2
-  const h2 = document.createElement("h2");
-  h2.id = "comic-title";
-  h2.className = "comic-title";
-  h2.textContent = title;
+//   //Comic Title H2
+//   const h2 = document.createElement("h2");
+//   h2.id = "comic-title";
+//   h2.className = "comic-title";
+//   h2.textContent = title;
 
-  //Comic Image
-  const comicImg = document.createElement("img");
-  comicImg.id = "comic-img";
-  comicImg.src = img;
+//   //Comic Image
+//   const comicImg = document.createElement("img");
+//   comicImg.id = "comic-img";
+//   comicImg.src = img;
 
-  //Transcript P
-  const p = document.createElement("p");
-  p.id = "transcript-text";
-  p.textContent = transcript;
+//   //Transcript P
+//   const p = document.createElement("p");
+//   p.id = "transcript-text";
+//   p.textContent = transcript;
 
-  //ChangeComic
-  const nextComicBtn = document.createElement("button");
-  nextComicBtn.id = "next-comic-btn";
-  nextComicBtn.className = "next-comic-btn";
-  nextComicBtn.textContent = "Next Comic";
-  comicAreaDiv.appendChild(nextComicBtn);
+//   //ChangeComic
+//   const nextComicBtn = document.createElement("button");
+//   nextComicBtn.id = "next-comic-btn";
+//   nextComicBtn.className = "next-comic-btn";
+//   nextComicBtn.textContent = "Next Comic";
+//   comicAreaDiv.appendChild(nextComicBtn);
 
-  //push to DOM
-  comicAreaDiv.append(h1, h2, comicImg, p, nextComicBtn);
+//   //push to DOM
+//   comicAreaDiv.append(h1, h2, comicImg, p, nextComicBtn);
+// };
+
+export const renderComicInfo = (comicAreaDiv, comic) => {
+  comicAreaDiv.innerHTML = `
+    <h2>${comic.title}</h2>
+    <img src="${comic.img}" alt="${comic.title}">
+    <p>${comic.transcript}</p>
+  `;
 };
