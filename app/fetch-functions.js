1614015665a2fb65f04a869f597b42df4ebe1d11
Wraymar <tyreesewray14@gmail.com>
2025-01-21 14:21:09 -0500

    fixed button issue

--- app/fetch-functions.js
+++ app/fetch-functions.js
@@ -1,15 +1,4 @@
 //FETCH COMIC
-export const fetchComic = async (comicNumber, comicAreaDiv, callback) => {
-  const url = `https://xkcd.vercel.app/?comic=${comicNumber}`;
-
-  await fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      callback(comicAreaDiv, data);
-    })
-    .catch((error) => console.error("Error fetching comic:", error));
-};
-
 export const getRandomComic = async () => {
   const randomComicNumber = Math.floor(Math.random() * 3038) + 1;
   const url = `https://xkcd.vercel.app/?comic=${randomComicNumber}`;
--- app/main.js
+++ app/main.js
@@ -1,17 +1,12 @@
-import './style.css';
-import { fetchRandomComic } from './fetch-functions';
-import { renderComicInfo } from './render-functions';
+import "./style.css";
+import { fetchRandomComic } from "./fetch-functions";
+import { renderComicInfo } from "./render-functions";
 
 const main = () => {
-  const comicArea = document.querySelector('#Comics-section');
+  const comicArea = document.querySelector("#Comics-section");
 
   // Fetch and render a random comic initially
   fetchRandomComic(comicArea, renderComicInfo);
-
-  const switchComic = document.querySelector('#next-comic-btn');
-  switchComic.addEventListener('click', () => {
-    fetchRandomComic(comicArea, renderComicInfo);
-  });
 };
 
 main();
